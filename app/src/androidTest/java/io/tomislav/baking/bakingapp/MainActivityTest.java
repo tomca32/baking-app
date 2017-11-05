ecf8a9ee68f1736d718cb04b4c366a851802d927
Tomislav Pesut <tomca32@gmail.com>
2017-11-05 11:01:31 -0500

    Added clicking test for MainActivity

--- app/src/androidTest/java/io/tomislav/baking/bakingapp/MainActivityTest.java
+++ app/src/androidTest/java/io/tomislav/baking/bakingapp/MainActivityTest.java
@@ -2,8 +2,11 @@ package io.tomislav.baking.bakingapp;
 
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.IdlingResource;
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import org.junit.After;
 import org.junit.Before;
@@ -12,10 +15,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static io.tomislav.baking.bakingapp.helpers.atPosition;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.allOf;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
@@ -34,8 +41,16 @@ public class MainActivityTest {
 
     @Test
     public void checkThatViewGetPopulatedWithRecipes() {
-        onView(withId(R.id.recipe_list)).check(matches(atPosition(0, hasDescendant(withId(R.id.recipe_name)))));
-        onView(withId(R.id.recipe_list)).check(matches(atPosition(0, hasDescendant(withId(R.id.recipe_image)))));
+        onView(withId(R.id.recipe_list)).check(matches(hasDescendant(withId(R.id.recipe_name))));
+        onView(withId(R.id.recipe_list)).check(matches(hasDescendant(withId(R.id.recipe_image))));
+    }
+
+    @Test
+    public void clickingOnRecipeCardShowsRecipeSteps() {
+        onView(withId(R.id.recipe_list)).perform(RecyclerViewActions.actionOnItem(hasDescendant(withText("Nutella Pie")), click()));
+
+        onView(allOf(isAssignableFrom(TextView.class), withParent(isAssignableFrom(Toolbar.class)))).check(matches(withText("Nutella Pie")));
+        onView(withId(R.id.step_list)).check(matches(hasDescendant(withId(R.id.description))));
     }
 
     @After
